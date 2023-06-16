1071e31c0598ce7a15141a666382d8e0fd61f3c8
Anthony Asp <46567012+AAspCodes@users.noreply.github.com>
2023-06-16 15:32:58 -0700

    refactor client.go

--- client/client.go
+++ client/client.go
@@ -12,23 +12,36 @@ import (
 func main() {
 	fmt.Println("Starting the client...")
 	// Dial a TCP connection to localhost on port 8080
+	conn := create_conn()
+
+	pod_name := os.Getenv("POD_NAME")
+
+	message := "hello from " + pod_name
+
+	send_message(conn, message)
+
+	conn.Close()
+	time.Sleep(5 * time.Second)
+	fmt.Println("client finished sending")
+}
+
+func create_conn() net.Conn {
 	conn, err := net.Dial("tcp", "server-service:80")
 	if err != nil {
 		log.Fatal(err)
 	}
+	return conn
+}
 
-	pod_name := os.Getenv("POD_NAME")
+func send_message(conn net.Conn, message string) {
 	// Message to send
-	jsonData, err := json.Marshal("hello from " + pod_name)
+	jsonData, err := json.Marshal(message)
 	if err != nil {
-		fmt.Println(err, "here")
+		fmt.Println(err, "client failed to send message")
 	}
 	// Send the message
 	_, err = conn.Write(jsonData)
 	if err != nil {
 		log.Fatal(err)
 	}
-	conn.Close()
-	time.Sleep(5 * time.Second)
-	fmt.Println("client finished sending")
 }
